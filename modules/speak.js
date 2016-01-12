45aae13abe1d9dc833f1252f07802e5b176652b8
Kenneth Fechter <kenneth.fechter@gmail.com>
2016-01-12 00:38:37 -0500

    add speak call function

--- modules/speak.js
+++ modules/speak.js
@@ -2,10 +2,13 @@ var say = require('say') // This module is designed for linux. Change the name t
 
 
 exports.speakMessage = function(sender, message){
-    
     say.speak('', 'Message from ' + sender, function () {
         setTimeout(function() {
             say.speak('', message);
         }, 3000);  
     });
+}
+
+exports.speakCall = function(caller){
+     say.speak('', 'Call from ' + caller);
 }
