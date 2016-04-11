140b5d3318a0a365a417328af939ba12f5336595
Kenneth Fechter <kenneth.fechter@gmail.com>
2016-04-11 00:53:57 -0400

    Add New items
    Out with the old, in with the new.

---
+++ controllers/modules/five.js
@@ -0,0 +1,188 @@
+var five = require('johnny-five');
+var colorMap = require('../../config/led-colors.js');
+var hardwareSupport = require('../../config/hardwareConfig');
+
+var leds;
+var board;
+var silentMode = false;
+var prevState = {
+    breatheMode : false,
+    rainbowMode : false,
+}
+
+var curColor = colorMap.color["cyan"];
+var nightMode = false;
+var lastIntensity = 0;
+var curIntensity = 0;
+var curPos = 0;
+var endPos = colorMap.rainbow.length;
+var bVal = 2;
+var breatheMode = true;
+var rainbowMode = true;
+var direction = "up";
+
+exports.initBoard = function(callback){
+    board = new five.Board();
+    board.on("ready", function() {
+        leds = new five.Led.RGB({
+        pins: {
+            red : hardwareSupport.redpin,
+            green: hardwareSupport.greenpin,
+            blue: hardwareSupport.bluepin,
+        }
+    });
+    console.log(leds);
+    this.repl.inject({
+       leds: leds,
+    });
+    
+    if(!silentMode){
+        leds.on();
+    leds.color(colorMap.color['green']);
+    setInterval(function() {
+        if (breatheMode) {
+            breathe(leds);
+        } else if (rainbowMode) {
+            rainbow(leds);
+        }
+    }, 90);
+    }  
+    callback();
+    });
+  
+}
+
+exports.notificationState = function(color){
+    if (!nightMode) {
+        prevState.breatheMode = breatheMode;
+        prevState.rainbowMode = rainbowMode;
+        breatheMode = false;
+        rainbowMode = false;
+        leds.on();
+        leds.intensity(100);
+        leds.color(colorMap.color[color]);
+        leds.strobe(500);
+    }
+};
+
+exports.idleState = function(){
+    breatheMode = prevState.breatheMode;
+    rainbowMode = prevState.rainBowMode;
+    leds.stop();
+    if (!nightMode) {
+        if (breatheMode) {
+            leds.intensity(curIntensity);
+        } else {
+            leds.intensity(bVal);
+        }
+        leds.color(curColor);
+    } 
+};
+
+var breathe = function(){
+    if (bVal > 0) {
+        if (lastIntensity >= bVal) {
+            direction = "down";
+        } else if (lastIntensity <= 0) {
+            direction = "up";
+        }
+
+        if (direction === "up") {
+            curIntensity = lastIntensity + 1;
+            lastIntensity = curIntensity;
+            leds.intensity(curIntensity);
+        } else {
+            curIntensity = lastIntensity - 1;
+            lastIntensity = curIntensity;
+            leds.intensity(curIntensity);
+        }
+    }
+};
+
+var rainbow = function(){
+      if (curPos >= endPos - 1) {
+            curPos = 0; // iterate to next color  
+        } else {
+            curPos++;
+        }
+        leds.color(colorMap.rainbow[curPos]);
+};
+
+exports.SetNightMode = function(nightModeSetting){
+    nightMode = nightModeSetting;
+    if(nightModeSetting){
+        console.log('Nightmode On Received');
+    } else {
+        leds.on();
+        console.log('NightMode Off Received');
+    }
+};
+
+exports.SetBreatheMode = function(breatheModeSetting){
+    breatheMode = breatheModeSetting;
+    prevState.breatheMode = breatheModeSetting;
+    if(breatheModeSetting){
+        rainbowMode = false
+        prevState.rainbowMode = false;
+        console.log('breatheMode On Received');
+    } else {
+      console.log('breatheMode Off Received'); 
+    }
+};
+
+exports.SetRainbowMode = function(rainbowModeSetting){
+    rainbowMode = rainbowModeSetting;
+    prevState.rainbowMode = rainbowModeSetting;
+    
+    if(rainbowMode){
+        breatheMode = false;
+        prevState.breatheMode = false;
+        console.log('rainbowMode On received')
+    } else{
+        breatheMode = true;
+        prevState.breatheMode = true;
+        console.log('rainbowMode Off received');
+    }
+};
+
+exports.SetIntensity = function(intensity){
+    bVal = intensity;
+    leds.intensity(bVal);
+};
+
+exports.SetColor = function(color){
+    curColor = color;
+    leds.color(curColor);
+    leds.intensity(bVal);
+};
+
+exports.SetTemp = function (temp){
+
+        if (temp <= 0) {
+            curColor = colorMap["white"];   
+        } else if ( temp > 0 && temp <= 35 ) { 
+            curColor = colorMap["icyblue"];
+        } else if ( temp > 35 && temp <= 50 ) {
+            curColor = colorMap["blue"];
+        } else if ( temp > 50 && temp <= 70) {
+            curColor = colorMap["yellow"];
+        } else if ( temp > 70 && temp <= 85) {
+            curColor = colorMap["orange"]; 
+        } else {
+            curColor = colorMap["red"];
+        }
+        leds.color(curColor);
+        leds.intensity(2);
+};
+
+exports.SetError = function(){
+    leds.color(colorMap.color["red"]);
+        leds.strobe(100);
+        console.log('error');
+};
+
+exports.setNop = function(){
+    if(!rainbowMode) {
+            this.idleState();
+        }
+};
---
+++ controllers/user.js
@@ -0,0 +1,235 @@
+var _ = require('lodash');
+var async = require('async');
+var crypto = require('crypto');
+var passport = require('passport');
+var User = require('../models/User');
+var secrets = require('../config/secrets');
+
+/**
+ * GET /login
+ * Login page.
+ */
+exports.getLogin = function(req, res) {
+  if (req.user) return res.redirect('/');
+  console.log('csrfToken = ' + req.csrfToken());
+  res.render('account/login', {
+    title: 'Login',
+    _csrf: req.csrfToken()
+  });
+};
+
+/**
+ * POST /login
+ * Sign in using email and password.
+ */
+exports.postLogin = function(req, res, next) {
+  req.assert('email', 'Email is not valid').isEmail();
+  req.assert('password', 'Password cannot be blank').notEmpty();
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/login');
+  }
+
+  passport.authenticate('local', function(err, user, info) {
+    if (err) return next(err);
+    if (!user) {
+      req.flash('errors', { msg: info.message });
+      return res.redirect('/login');
+    }
+    req.logIn(user, function(err) {
+      if (err) return next(err);
+      req.flash('success', { msg: 'Success! You are logged in.' });
+      res.redirect(req.session.returnTo || '/');
+    });
+  })(req, res, next);
+};
+
+/**
+ * GET /logout
+ * Log out.
+ */
+exports.logout = function(req, res) {
+  req.logout();
+  res.redirect('/');
+};
+
+/**
+ * GET /signup
+ * Signup page.
+ */
+exports.getSignup = function(req, res) {
+  if (req.user) return res.redirect('/');
+  res.render('account/setup', {
+    title: 'Create Account',
+    _csrf: req.csrfToken
+  });
+};
+
+/**
+ * POST /signup
+ * Create a new local account.
+ */
+exports.postSignup = function(req, res, next) {
+  req.assert('email', 'Email is not valid').isEmail();
+  req.assert('password', 'Password must be at least 4 characters long').len(4);
+  req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/');
+  }
+
+  var user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  User.findOne({ email: req.body.email }, function(err, existingUser) {
+    if (existingUser) {
+      req.flash('errors', { msg: 'Account with that email address already exists.' });
+      return res.redirect('/setup');
+    }
+    user.save(function(err) {
+      if (err) return next(err);
+      req.logIn(user, function(err) {
+        if (err) return next(err);
+        res.redirect('/');
+      });
+    });
+  });
+};
+
+/**
+ * GET /account
+ * Profile page.
+ */
+exports.getAccount = function(req, res) {
+  res.render('account/profile', {
+    title: 'Account Management',
+    _csrf: req.csrfToken()
+  });
+};
+
+/**
+ * POST /account/profile
+ * Update profile information.
+ */
+exports.postUpdateProfile = function(req, res, next) {
+  User.findById(req.user.id, function(err, user) {
+    if (err) return next(err);
+    user.email = req.body.email || '';
+    user.profile.name = req.body.name || '';
+    user.profile.gender = req.body.gender || '';
+    user.profile.location = req.body.location || '';
+    user.profile.website = req.body.website || '';
+
+    user.save(function(err) {
+      if (err) return next(err);
+      req.flash('success', { msg: 'Profile information updated.' });
+      res.redirect('/account');
+    });
+  });
+};
+
+/**
+ * POST /account/password
+ * Update current password if changed, and update the profile
+ */
+exports.postUpdateDashboard = function(req, res, next){
+    if(req.body.password.len > 0){
+         req.assert('password', 'Password must be at least 4 characters long').len(4);
+         req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
+         req.assert('')
+    }
+};
+
+/**
+ * POST /account/password
+ * Update current password.
+ */
+exports.postUpdatePassword = function(req, res, next) {
+ 
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/account');
+  }
+
+  User.findById(req.user.id, function(err, user) {
+    if (err) return next(err);
+
+    user.password = req.body.password;
+
+    user.save(function(err) {
+      if (err) return next(err);
+      req.flash('success', { msg: 'Password has been changed.' });
+      res.redirect('/account');
+    });
+  });
+};
+
+/**
+ * GET /reset/:token
+ * Reset Password page.
+ */
+exports.getReset = function(req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/');
+  } else {
+      res.render('account/reset', {
+        title: 'Password Reset',
+        _csrf: req.csrfToken()
+      });
+  }
+};
+
+/**
+ * POST /reset/:token
+ * Process the reset password request.
+ */
+exports.postReset = function(req, res, next) {
+  req.assert('email',"Email must not be blank").notEmpty();
+  req.assert('password', 'Password must be at least 4 characters long.').len(4);
+  req.assert('confirm', 'Passwords must match.').equals(req.body.password);
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('back');
+  }
+
+  async.waterfall([
+    function(done) {
+      User.findOne({ email: req.body.email })
+        .exec(function(err, user) {
+          if (!user) {
+            req.flash('errors', { msg: 'An error has occurred.' });
+            return res.redirect('back');
+          }
+
+          user.password = req.body.password;
+          user.save(function(err) {
+            if (err) return next(err);
+            req.logIn(user, function(err) {
+              done(err, user);
+            });
+          });
+        });
+    },
+    function(user, done) {
+        req.flash('success', { msg: 'Success! Your password has been changed.' });
+        done(err);
+      }
+  ], function(err) {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+};
